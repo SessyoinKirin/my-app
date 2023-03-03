98df7b50ff3dec4cbce949cb9ffdafe1a3dc02c3
Haohui Chen <neosdiva@gmail.com>
2023-03-03 14:43:23 -0300

    hoje foi uma pequena contextualização sobre o ciclo de vida, ComponentWillmount, DidMount, e render

---
+++ src/02-advance/12-ciclodevidainit.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+state={
+    myname:"zero"
+}
+
+    componentWillMount() {
+        console.log('willMount', this.state.myname, document.getElementById("myname"))
+
+        this.setState({
+            myname:"zero"
+        })
+
+        //inicialização de dados
+    }
+    componentDidMount() {
+
+        // requisita de dados de axios
+        // subscribe funções
+        // setinterval
+        // inicializa dom
+        console.log('didmount')
+    }
+
+    render() {
+        console.log('render')
+        return (
+            <div>
+                <span id="myname">{this.state.myname}</span>
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
  * @Author: SessyoinChen
  * @Date: 2022-07-17 20:58:26
  * @LastEditors: SessyoinChen
- * @LastEditTime: 2023-03-02 09:33:12
+ * @LastEditTime: 2023-03-03 14:22:01
  * @FilePath: \my-app\src\index.js
  * @Description: 05 - linguagem JSX
  * 
  */
 import React from 'react'
 import ReactDom from 'react-dom'
-import App from './02-advance/10-slot'
+import App from './02-advance/12-ciclodevidainit'
 
 ReactDom.render(
     // <React.StrictMode>
