720880623db0569d75ed762898c62f5502db1b17
Haohui Chen <neosdiva@gmail.com>
2023-03-06 13:14:01 -0300

    assunto sobre shoulcomponentupdate

---
+++ src/02-advance/15-ciclodevidaupdatep2.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+    state = {
+        myname: "natsu"
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={() => {
+                    this.setState({
+                        myname: "Gray"
+                    })
+                }}>click</button>
+                {this.state.myname}
+            </div>
+        )
+    }
+shouldComponentUpdate(nextProps, nextState){
+    if(JSON.stringify(this.state) !== JSON.stringify(nextState)){
+       return true 
+    }
+
+    return false
+}
+    componentDidUpdate(){
+        console.log("update")
+    }
+}
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@
  */
 import React from 'react'
 import ReactDom from 'react-dom'
-import App from './02-advance/14-ciclodevidaupdatep1'
+import App from './02-advance/15-ciclodevidaupdatep2'
 
 ReactDom.render(
     // <React.StrictMode>
