ff22ecf7b427def1e13f4a14860c4b10ff8a1ec5
Haohui Chen <neosdiva@gmail.com>
2023-03-12 21:58:47 -0300

    swiper assincrona e sincrona

---
+++ src/02-advance/26-swiperAssincro.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import Swiper, { Navigation, Pagination } from 'swiper'
+import 'swiper/swiper-bundle.min.css'
+Swiper.use([Navigation, Pagination])
+
+export default class App extends Component {
+    state = {
+        list: []
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                list: ['111', '222', '333']
+            })
+
+            new Swiper(".swiper", {
+                pagination:{
+                  el:'.swiper-pagination',
+                },
+              })
+        }, 1000)
+
+    }
+
+    // componentDidUpdate() {
+    //     new Swiper(".swiper", {
+    //         pagination: {
+    //             el: '.swiper-pagination',
+    //         },
+    //     })
+    // }
+
+    render() {
+        return (
+            <div>
+
+                <div className="swiper" style={{ height: "200px", background: "yellow" }}>
+
+                    <div className="swiper-wrapper">
+
+                        {
+                            this.state.list.map(item => <div className='swiper-slide' key={item}>
+                                {item}
+                            </div>)
+                        }
+
+                    </div>
+
+                    <div className="swiper-pagination"></div>
+
+                    {/* <div className="swiper-button-prev"></div>
+          <div className="swiper-button-next"></div>
+
+          <div className="swiper-scrollbar"></div> */}
+                </div>
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
  * @Author: SessyoinChen
  * @Date: 2022-07-17 20:58:26
  * @LastEditors: SessyoinChen
- * @LastEditTime: 2023-03-07 15:30:44
+ * @LastEditTime: 2023-03-12 21:53:33
  * @FilePath: \my-app\src\index.js
  * @Description: 05 - linguagem JSX
  * 
  */
 import React from 'react'
 import ReactDom from 'react-dom'
-import App from './02-advance/24-otimization'
+import App from './02-advance/26-swiperAssincro'
 
 ReactDom.render(
     // <React.StrictMode>
