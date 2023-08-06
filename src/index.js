ef77597bad478d2a34b0b6d9a9789f4929ced396
SessyoinChen <haohui.chen.alter@gmail.com>
2023-08-06 12:09:37 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@
  * @Author: SessyoinChen
  * @Date: 2022-07-17 20:58:26
  * @LastEditors: SessyoinChen
- * @LastEditTime: 2023-04-20 10:09:13
+ * @LastEditTime: 2023-08-06 12:09:05
  * @FilePath: \my-app\src\index.js
  * @Description: 05 - linguagem JSX
  * 
  */
 import React from 'react'
 import ReactDom from 'react-dom'
-import App from './03-hooks/15-useReducer3'
+import App from './03-hooks/01-useState'
 
 ReactDom.render(
     // <React.StrictMode>
-        <App/>
+    <App />
     // </React.StrictMode>
-        ,document.getElementById("root")
+    , document.getElementById("root")
 )
