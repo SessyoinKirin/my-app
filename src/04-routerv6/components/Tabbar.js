9134a7e72c3926e9f47daf3a9854c7aae395d36d
SessyoinKirin <neosdiva@gmail.com>
2023-10-14 20:48:17 -0300

    uso de navlink ao invés de link, passando props de isActive para evitar usar palavra chave 'active' no ambiente de desenvolvimento

--- src/04-routerv6/components/Tabbar.js
+++ src/04-routerv6/components/Tabbar.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
+import './Tabbar.css'
+
 export default function Tabbar() {
   return (
     <div>
       <footer>
         <ul>
             <li>
-                <Link to='/film'>Filme</Link>
+                <NavLink to='/film' className={({isActive})=>isActive?'tabbarActive':''}>Filme</NavLink>
+            </li>
+            <li>
+                <NavLink to='/cinema' className={({isActive})=>isActive?'tabbarActive':''}>Cinema</NavLink>
+            </li>
+            <li>
+                <NavLink to='/center' className={({isActive})=>isActive?'tabbarActive':''}>Center</NavLink>
             </li>
         </ul>
       </footer>
