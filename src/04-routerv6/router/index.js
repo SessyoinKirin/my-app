9c92b613a52a63a95cbf1d14e4d9c1dcaf744992
SessyoinKirin <neosdiva@gmail.com>
2023-10-15 20:06:05 -0300

    usando lazyload para resolver problema de delay ao carregar a página na primeira vez, pois não vão mais instalar todos os arquivos e sim apenas uma só que necessita

--- src/04-routerv6/router/index.js
+++ src/04-routerv6/router/index.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Route, Routes, Navigate } from 'react-router-dom'
-import Film from '../views/Film'
-import Cinema from '../views/Cinema'
-import Center from '../views/Center'
-import Notfound from '../views/Notfound'
-import Search from '../views/Search'
-import NowPlaying from '../views/Films/NowPlaying'
-import ComingSoon from '../views/Films/ComingSoon'
+// import Film from '../views/Film'
+// import Cinema from '../views/Cinema'
+// import Center from '../views/Center'
+// import Notfound from '../views/Notfound'
+// import Search from '../views/Search'
+// import NowPlaying from '../views/Films/NowPlaying'
+// import ComingSoon from '../views/Films/ComingSoon'
 import Redirect from '../components/Redirect'
-import Detail from '../views/Detail'
-import Login from '../views/Login'
+// import Detail from '../views/Detail'
+// import Login from '../views/Login'
 
 export default function MRouter() {
     return (
@@ -17,22 +17,22 @@ export default function MRouter() {
             <Routes>
                 {/* <Route path="/" element={<Film />} /> */}
                 {/* <Route index element={<Film />} /> */}
-                <Route path="/film" element={<Film />} >
-                    <Route path='' element={<Redirect to="/film/nowPlaying" />} />
-                    <Route path='nowPlaying' element={<NowPlaying />} />
-                    <Route path='comingSoon' element={<ComingSoon />} />
+                <Route path="/film" element={lazyLoad('Film')} >
+                    <Route path='' element={<Redirect to="/Films/nowPlaying" />} />
+                    <Route path='nowPlaying' element={lazyLoad('Films/NowPlaying')} />
+                    <Route path='comingSoon' element={lazyLoad('Films/ComingSoon')} />
                 </Route>
-                <Route path="/cinema" element={<Cinema />} />
-                <Route path="/cinema/search" element={<Search />} />
+                <Route path="/cinema" element={lazyLoad('Cinema')} />
+                <Route path="/cinema/search" element={lazyLoad('Search')} />
                 {/* <Route path="/center" element={()=>{isAuth() ? <Center /> : <Redirect to='/login/' />}} /> */}
                 <Route path="/center" element={<AuthComponent>
-                    <Center></Center>
+                    {lazyLoad('Center')}
                 </AuthComponent>} />
-                <Route path='/login' element={<Login />}></Route>
-                <Route path="/detail/:myid" element={<Detail />} />
+                <Route path='/login' element={lazyLoad('Login')}/>
+                <Route path="/detail/:myid" element={lazyLoad('Detail')} />
 
                 <Route path="/" element={<Navigate to="/film" />} />
-                <Route path="*" element={<Notfound />} />
+                <Route path="*" element={lazyLoad('Notfound')} />
                 {/* <Route path="*" element={<Redirect to="/film"/>}/> */}
             </Routes>
         </div>
@@ -41,5 +41,14 @@ export default function MRouter() {
 
 function AuthComponent({ children }) {
     const isLogin = localStorage.getItem('token')
-    return isLogin?children:<Redirect to='/Login'/>
+    return isLogin ? children : <Redirect to='/Login' />
+}
+
+const lazyLoad = (path) => {
+    const Comp = React.lazy(() => import(`../views/${path}`))
+    return (
+        <React.Suspense fallback={<>Carregando</>}>
+            <Comp />
+        </React.Suspense>
+    )
 }
